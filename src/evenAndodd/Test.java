888427d7f25951d03a86964eb0fe366c97f9c33d
AkshayWankar <akkiwnkr358@gmail.com>
2023-01-11 12:41:19 +0530

    even and odd number

---
+++ src/evenAndodd/Test.java
@@ -0,0 +1,25 @@
+package evenAndodd;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		int a[]= {1,2,3,4,5,6};
+	
+		System.out.println("even number...");
+		
+		for(int i=0; i<a.length;i++)
+		{
+			if(a[i]%2==0)
+				System.out.println(a[i]);
+		}
+		
+		System.out.println("odd number...");
+		 for(int i=0; i<a.length;i++) 
+		 {
+			 if(a[i]%2!=0)
+				 System.out.println(a[i]);
+		 }
+	}
+	
+}
